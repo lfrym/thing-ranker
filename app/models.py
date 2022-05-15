d56ed62a03231aa50359c0cf98af14ce5c5059b9
lfrym <37262932+lfrym@users.noreply.github.com>
2022-05-15 11:21:40 -0500

    initial tests

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Nouns(models.Model):
+    noun_text = models.CharField(max_length=200)
+
+class Votes(models.Model):
+    noun = models.ForeignKey(Nouns, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
